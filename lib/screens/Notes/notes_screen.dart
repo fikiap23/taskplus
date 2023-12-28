71e6423283bf52467d591a23492f240d40c9a250
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-28 11:56:25 +0700

    add notes

--- lib/screens/Notes/notes_screen.dart
+++ lib/screens/Notes/notes_screen.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:taskplus/models/note_model.dart';
-import 'package:taskplus/common/constants/colors.dart';
 import 'package:taskplus/screens/Notes/edit_note_screen.dart';
 import 'package:taskplus/common/widgets/drawer_menu.dart';
 
@@ -15,97 +14,58 @@ class NotesScreen extends StatefulWidget {
 }
 
 class _NotesScreenState extends State<NotesScreen> {
-  List<NoteModel> filteredNotes = [];
-  bool sorted = false;
-
-  @override
-  void initState() {
-    super.initState();
-    filteredNotes = sampleNotes;
-  }
-
-  List<NoteModel> sortNotesByModifiedTime(List<NoteModel> notes) {
-    if (sorted) {
-      notes.sort((a, b) => a.modifiedTime.compareTo(b.modifiedTime));
-    } else {
-      notes.sort((b, a) => a.modifiedTime.compareTo(b.modifiedTime));
-    }
-
-    sorted = !sorted;
-
-    return notes;
-  }
-
-  getRandomColor() {
-    Random random = Random();
-    return backgroundColors[random.nextInt(backgroundColors.length)];
-  }
-
-  void onSearchTextChanged(String searchText) {
-    setState(() {
-      filteredNotes = sampleNotes
-          .where((note) =>
-              note.content.toLowerCase().contains(searchText.toLowerCase()) ||
-              note.title.toLowerCase().contains(searchText.toLowerCase()))
-          .toList();
-    });
-  }
-
-  void deleteNote(int index) {
-    setState(() {
-      NoteModel note = filteredNotes[index];
-      sampleNotes.remove(note);
-      filteredNotes.removeAt(index);
-    });
-  }
+  List<NoteModel> sampleNotes = [
+    NoteModel(
+      id: 0,
+      title: 'Tugas PAM',
+      content:
+          'Bikin aplikasi menggunakan flutter, kemudian deploy ke playstore',
+      modifiedTime: DateTime(2022, 1, 1, 3, 45),
+    ),
+    NoteModel(
+      id: 1,
+      title: 'Tugas Database',
+      content: 'Desain dan implementasikan skema database untuk proyek',
+      modifiedTime: DateTime(2022, 1, 2, 9, 30),
+    ),
+    NoteModel(
+      id: 2,
+      title: 'Tugas UI/UX',
+      content: 'Rancang antarmuka pengguna yang menarik dan responsif',
+      modifiedTime: DateTime(2022, 1, 3, 12, 15),
+    ),
+    NoteModel(
+      id: 3,
+      title: 'Tugas Testing',
+      content:
+          'Bangun dan jalankan rangkaian uji untuk memastikan aplikasi berfungsi dengan baik',
+      modifiedTime: DateTime(2022, 1, 4, 14, 0),
+    ),
+    NoteModel(
+      id: 4,
+      title: 'Tugas Dokumentasi',
+      content: 'Dokumentasikan setiap bagian dari proyek secara lengkap',
+      modifiedTime: DateTime(2022, 1, 5, 16, 45),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.grey.shade900,
       appBar: AppBar(title: const Text("TaskPlus")),
       drawer: const DrawerMenu(),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Notes',
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                ),
-                IconButton(
-                    onPressed: () {
-                      setState(() {
-                        filteredNotes = sortNotesByModifiedTime(filteredNotes);
-                      });
-                    },
-                    padding: const EdgeInsets.all(0),
-                    icon: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                          color: Color(0xFF4B6AAB),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: const Icon(
-                        Icons.sort,
-                        color: Colors.white,
-                      ),
-                    ))
-              ],
-            ),
             const SizedBox(
               height: 20,
             ),
             TextField(
-              onChanged: onSearchTextChanged,
               style: const TextStyle(fontSize: 16, color: Colors.white),
               decoration: InputDecoration(
                 contentPadding: const EdgeInsets.symmetric(vertical: 12),
                 hintText: "Search notes...",
-                // hintStyle: const TextStyle(color: Colors.grey),
                 prefixIcon: const Icon(
                   Icons.search,
                   color: Colors.white,
@@ -126,115 +86,85 @@ class _NotesScreenState extends State<NotesScreen> {
               height: 20,
             ),
             Expanded(
-                child: ListView.builder(
-              padding: const EdgeInsets.only(top: 30),
-              itemCount: filteredNotes.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  color: getRandomColor(),
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: ListTile(
-                      onTap: () async {
-                        final result = await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                EditScreen(note: filteredNotes[index]),
-                          ),
-                        );
-                        if (result != null) {
-                          setState(() {
-                            int originalIndex =
-                                sampleNotes.indexOf(filteredNotes[index]);
-
-                            sampleNotes[originalIndex] = NoteModel(
-                                id: sampleNotes[originalIndex].id,
-                                title: result[0],
-                                content: result[1],
-                                modifiedTime: DateTime.now());
-
-                            filteredNotes[index] = NoteModel(
-                                id: filteredNotes[index].id,
-                                title: result[0],
-                                content: result[1],
-                                modifiedTime: DateTime.now());
-                          });
-                        }
-                      },
-                      title: RichText(
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        text: TextSpan(
-                            text: '${filteredNotes[index].title} \n',
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 30),
+                itemCount: sampleNotes.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    color: getRandomColor(),
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        onTap: () async {
+                          // Handle onTap
+                        },
+                        title: RichText(
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          text: TextSpan(
+                            text: '${sampleNotes[index].title} \n',
                             style: const TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                height: 1.5),
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              height: 1.5,
+                            ),
                             children: [
                               TextSpan(
-                                text: filteredNotes[index].content,
+                                text: sampleNotes[index].content,
                                 style: const TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 14,
-                                    height: 1.5),
-                              )
-                            ]),
-                      ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: Text(
-                          'Edited: ${DateFormat('EEE MMM d, yyyy h:mm a').format(filteredNotes[index].modifiedTime)}',
-                          style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 14,
+                                  height: 1.5,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            'Edited: ${DateFormat('EEE MMM d, yyyy h:mm a').format(sampleNotes[index].modifiedTime)}',
+                            style: TextStyle(
                               fontSize: 10,
                               fontStyle: FontStyle.italic,
-                              color: Colors.grey.shade800),
+                              color: Colors.grey.shade800,
+                            ),
+                          ),
                         ),
-                      ),
-                      trailing: IconButton(
-                        onPressed: () async {
-                          final result = await confirmDialog(context);
-                          if (result != null && result) {
-                            deleteNote(index);
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.delete,
+                        trailing: IconButton(
+                          onPressed: () async {
+                            // Handle delete
+                            confirmDialog(context);
+                          },
+                          icon: const Icon(
+                            Icons.delete,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ))
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          final result = await Navigator.push(
+          // Handle floating action button
+          Navigator.push(
             context,
             MaterialPageRoute(
               builder: (BuildContext context) => const EditScreen(),
             ),
           );
-
-          if (result != null) {
-            setState(() {
-              sampleNotes.add(NoteModel(
-                  id: sampleNotes.length,
-                  title: result[0],
-                  content: result[1],
-                  modifiedTime: DateTime.now()));
-              filteredNotes = sampleNotes;
-            });
-          }
         },
         elevation: 10,
         backgroundColor: Color(0xFF4B6AAB),
@@ -244,6 +174,11 @@ class _NotesScreenState extends State<NotesScreen> {
         ),
       ),
     );
+  }
+
+  getRandomColor() {
+    Random random = Random();
+    return Colors.primaries[random.nextInt(Colors.primaries.length)];
   }
 
   Future<dynamic> confirmDialog(BuildContext context) {
